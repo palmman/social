96bcff0b376d27fb3983518ccd98129e24387daa
Worawit Boonsorn <palm454555@hotmail.com>
2021-10-28 23:09:15 +0700

    first commit

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+
+    path('settings/', views.settings, name='settings'),
+    path('upload/', views.upload, name='upload'),
+    path('like/', views.like, name='like'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('follow/', views.follow, name='follow'),
+]
